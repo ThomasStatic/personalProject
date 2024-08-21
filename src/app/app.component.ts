d48c90773133e90238c9a64f5455987a62da3093
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-08-20 22:07:31 -0400

    create home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { UsingThisSiteComponent } from './home-page/using-this-site/using-this-site.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent, HomePageComponent],
+  imports: [RouterOutlet, NavigationBarComponent, HomePageComponent, UsingThisSiteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/home-page/using-this-site/using-this-site.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-using-this-site',
+  standalone: true,
+  imports: [],
+  templateUrl: './using-this-site.component.html',
+  styleUrl: './using-this-site.component.css'
+})
+export class UsingThisSiteComponent {
+
+}
